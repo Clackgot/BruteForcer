24122d1e9fe76141bead3b8bd3296ef0ab36ec68
clackgot <clackgot@gmail.com>
2021-02-02 17:41:14 +0300

    Добавьте файлы проекта.

---
+++ InstagramSaver/Program.cs
@@ -0,0 +1,30 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Parser;
+using AngleSharp.Io;
+using System;
+using System.Threading.Tasks;
+
+namespace InstagramSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Open().Wait();
+        }
+
+        private static async Task Open()
+        {
+            var config = Configuration.Default
+    .WithDefaultCookies()
+    .WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync("https://www.kinopoisk.ru/lists/top250/");
+
+            foreach (var name in document.QuerySelectorAll("p.selection-film-item-meta__name"))
+            {
+                Console.WriteLine(name.TextContent);
+            }
+        }
+    }
+}
