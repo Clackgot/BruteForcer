ebd99659fe3870052d97f8f0349a28193bdc9b91
clackgot <clackgot@gmail.com>
2021-02-06 19:37:40 +0300

    Added project FixPriceEmailChecker

---
+++ FixPriceEmailChecker/Program.cs
@@ -0,0 +1,85 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Io;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FixPriceEmailChecker
+{
+
+    class Checker
+    {
+        private IBrowsingContext context;
+        private List<string> emails = new List<string>();
+        public Checker(string emailDataPath)
+        {
+            var config = Configuration.Default
+.WithDefaultCookies()
+.WithDefaultLoader();
+            context = BrowsingContext.New(config);
+            loadEmails(emailDataPath);
+        }
+
+        private void loadEmails(string path)
+        {
+            StreamReader sr = new StreamReader(path);//Считыватель потока
+            string line;//Одна строка из файла
+            while (!sr.EndOfStream)//Пока не конец файла
+            {
+                line = sr.ReadLine();//Считать строку
+                emails.Add(line);//Добавить это строку в список паролей
+            }
+            sr.Close();//Закрыть поток
+            Console.WriteLine($"Загружено {emails.Count} паролей");
+        }
+
+        public void Check(List<string> emails)
+        {
+            List<Task<IDocument>> response = new List<Task<IDocument>>();
+            foreach (var email in emails)
+            {
+                response.Add(mailConfirm(email));
+            }
+            Task.WaitAll(response.ToArray());
+        }
+
+        private async Task<IDocument> mailConfirm(string email)
+        {
+            var dictonary = new Dictionary<string, string>();
+            dictonary.Add(@"mail_confirm", "Y");
+            dictonary.Add(@"action", "getCode");
+            dictonary.Add(@"email", email);
+            var documentRequest = DocumentRequest.PostAsUrlencoded(new Url("https://fix-price.ru/ajax/confirm_mail.php"),
+                dictonary);
+            var result = await context.OpenAsync(documentRequest);
+            Console.WriteLine(unicodeEncode(result.Source.Text));
+            return result;
+        }
+        private static string unicodeEncode(string text)
+        {
+            var rx = new Regex(@"\\u([0-9A-Z]{4})", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return rx.Replace(text, p => new string((char)int.Parse(p.Groups[1].Value, NumberStyles.HexNumber), 1));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Checker checker = new Checker("emails.txt");
+            List<string> emails = new List<string>()
+            {
+                "clackgot@gmail.com",
+                "sdfsdfsdf@gmail.com",
+                "sdssdf@gmail.com",
+                "f12312sdfsd@gmail.com",
+                "sdfsdf@gmail.com",
+            };
+            checker.Check(emails);
+        }
+    }
+}
